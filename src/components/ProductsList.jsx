1ed0dabea26484a827df3ee51bd334f28143762f
nicola <you@example.com>
2023-09-22 12:56:52 +0100

    added home page

---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+
+const Productslist = ()=>{
+const[isLoading, setIsLoading] = useState("");
+return (
+    <p> Products</p>
+)
+}
+
+
+export default Productslist
